39b6fea03085b54c2a3368571a95d1ec38bfb8aa
Abhishek Hanamagond <iabhishekhanamagond@gmail.com>
2025-09-20 18:51:17 +0530

    db migration

--- Migrations/20250920131822_MadeSellerIdOptional12.Designer.cs
+++ Migrations/20250920131822_MadeSellerIdOptional12.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPersonalWebsite.Models;
 
@@ -11,9 +12,11 @@ using MyPersonalWebsite.Models;
 namespace MyPersonalWebsite.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250920131822_MadeSellerIdOptional12")]
+    partial class MadeSellerIdOptional12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
