00768cf0db1a2ccb12e4c6e07073e323f9c091e3
thenameisabhi1289 <thenameisabhi1289@gmail.com>
2025-09-20 12:30:03 +0000

    dabase issue

--- Program.cs
+++ Program.cs
@@ -7,29 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlOptions => sqlOptions.EnableRetryOnFailure()
-    ));
+   options.UseSqlServer(
+       builder.Configuration.GetConnectionString("DefaultConnection"),
+       sqlOptions => sqlOptions.EnableRetryOnFailure()
+   ));
 
 
 var app = builder.Build();
-
-// Create DB on startup
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<AppDbContext>();
-        context.Database.EnsureCreated();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred creating the DB.");
-    }
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
