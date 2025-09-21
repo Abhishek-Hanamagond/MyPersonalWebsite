e8547e47db195283c6af4e1508eace42d40980b6
Abhishek Hanamagond <iabhishekhanamagond@gmail.com>
2025-09-21 13:55:44 +0530

    migrations

---
+++ Migrations/20250920185410_MadeSellerIdOptional-change1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPersonalWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class MadeSellerIdOptionalchange1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
